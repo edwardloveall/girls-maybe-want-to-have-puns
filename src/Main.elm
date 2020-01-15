3359419dcf54ead8df5a1342799aa30e870806a9
Edward Loveall <edward@edwardloveall.com>
2020-01-14 22:20:13 -0500

    Print phrases along with rhymes

--- src/Main.elm
+++ src/Main.elm
@@ -6,6 +6,36 @@ import Http exposing (Error)
 import Json.Decode exposing (Decoder, field, int, list, map2, string)
 
 
+type RemoteData a
+    = Loading
+    | Failure
+    | Success a
+
+
+type alias Rhyme =
+    { word : String
+    , score : Int
+    }
+
+
+type alias RhymeResult =
+    RemoteData (List Rhyme)
+
+
+type Phrase
+    = Phrase String
+
+
+type alias PhraseResult =
+    RemoteData (List Phrase)
+
+
+type alias Model =
+    { rhymes : RhymeResult
+    , phrases : PhraseResult
+    }
+
+
 main : Program () Model Msg
 main =
     Browser.element
@@ -16,37 +46,34 @@ main =
         }
 
 
-type alias Rhyme =
-    { word : String
-    , score : Int
-    }
-
-
-type Model
-    = Failure
-    | Loading
-    | Success (List Rhyme)
-
-
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Loading, getRhymes )
+    ( Model Loading Loading, Cmd.batch [ getRhymes, getPhrases ] )
 
 
 type Msg
     = GotRhymes (Result Error (List Rhyme))
+    | GotPhrases (Result Error String)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg _ =
+update msg model =
     case msg of
         GotRhymes result ->
             case result of
                 Ok rhymes ->
-                    ( Success rhymes, Cmd.none )
+                    ( { model | rhymes = Success rhymes }, Cmd.none )
 
                 Err _ ->
-                    ( Failure, Cmd.none )
+                    ( { model | rhymes = Failure }, Cmd.none )
+
+        GotPhrases result ->
+            case result of
+                Ok phrases ->
+                    ( { model | phrases = String.split "\n" phrases |> List.map Phrase |> Success }, Cmd.none )
+
+                Err _ ->
+                    ( { model | phrases = Failure }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -61,7 +88,13 @@ view model =
 
 viewPuns : Model -> Html Msg
 viewPuns model =
-    case model of
+    div []
+        [ rhymeView model.rhymes, phraseView model.phrases ]
+
+
+rhymeView : RhymeResult -> Html Msg
+rhymeView rhymeResult =
+    case rhymeResult of
         Failure ->
             text "It failed"
 
@@ -69,10 +102,24 @@ viewPuns model =
             text "Loading..."
 
         Success rhymes ->
-            div []
-                (rhymeList
-                    rhymes
-                )
+            rhymeList
+                rhymes
+                |> div []
+
+
+phraseView : PhraseResult -> Html Msg
+phraseView phraseResult =
+    case phraseResult of
+        Failure ->
+            text "It failed"
+
+        Loading ->
+            text "Loading..."
+
+        Success phrases ->
+            List.map phraseToParagraph
+                phrases
+                |> div []
 
 
 rhymeList : List Rhyme -> List (Html Msg)
@@ -99,6 +146,15 @@ rhymeToParagraph rhyme =
     p [] [ text rhyme.word ]
 
 
+phraseToParagraph : Phrase -> Html Msg
+phraseToParagraph phrase =
+    let
+        (Phrase magic) =
+            phrase
+    in
+    p [] [ text magic ]
+
+
 getRhymes : Cmd Msg
 getRhymes =
     Http.get
@@ -115,3 +171,11 @@ rhymesDecoder =
 rhymeDecoder : Decoder Rhyme
 rhymeDecoder =
     map2 Rhyme (field "word" string) (field "score" int)
+
+
+getPhrases : Cmd Msg
+getPhrases =
+    Http.get
+        { url = "/data/wikipedia-idioms.txt"
+        , expect = Http.expectString GotPhrases
+        }
